efd9a37ac10ec089609a850c406cff1915a3d870
kapi1121 <157882257+kapi1121@users.noreply.github.com>
2024-05-04 00:14:05 +0900

    ユーザー管理機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
-  root to:"items#index"
-  resources :users, only: [:edit, :update]
-  resources :items, only: :index
+  root to: "items#index"
+  resources :users, only: [:show, :edit, :update]
+  resources :items, only: [:index, :new]
 end
