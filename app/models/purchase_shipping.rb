7971e363a2b9c97bce6ecbc237c60d7a3e70007a
kapi1121 <157882257+kapi1121@users.noreply.github.com>
2024-06-04 13:46:32 +0900

    エラー表示の実装

--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -2,13 +2,13 @@ class PurchaseShipping
   include ActiveModel::Model
   attr_accessor :post_code, :prefecture_id, :municipality, :street_address, :building_name, :phone_number, :user_id, :item_id
 
-  validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  validates :prefecture_id, presence: true, numericality: { other_than: 0, message: "can't be blank" }
-  validates :municipality, presence: true
-  validates :street_address, presence: true
-  validates :phone_number, presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}-[0-9]{4}\z/ }, length: { maximum: 11 }
-  validates :user_id, presence: true
-  validates :item_id, presence: true
+  validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+  validates :prefecture_id, presence: { message: "can't be blank" }, numericality: { other_than: 0, message: "can't be blank" }
+  validates :municipality, presence: { message: "can't be blank" }
+  validates :street_address, presence: { message: "can't be blank" }
+  validates :phone_number, presence: { message: "can't be blank" }, format: { with: /\A[0-9]{3}-[0-9]{4}-[0-9]{4}\z/ }, length: { maximum: 11 }
+  validates :user_id, presence: { message: "can't be blank" }
+  validates :item_id, presence: { message: "can't be blank" }
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
