02e4464dd645d43938c235d07065a999328666d0
kapi1121 <157882257+kapi1121@users.noreply.github.com>
2024-06-09 20:58:21 +0900

    売却済み商品のページ遷移の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -52,5 +53,11 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def correct_user
+    if current_user.id != @item.user_id || @item.purchase.present?
+      redirect_to root_path
+    end
   end
 end
